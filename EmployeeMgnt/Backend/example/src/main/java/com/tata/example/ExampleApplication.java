5030f42032f2b104056f241ea42be4e1ef7faa2d
Christopher Davisson <davissonchris@gmail.com>
2022-01-31 10:58:42 -0800

    first commit

---
+++ EmployeeMgnt/Backend/example/src/main/java/com/tata/example/ExampleApplication.java
@@ -0,0 +1,31 @@
+package com.tata.example;
+
+import com.tata.example.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class ExampleApplication  {
+
+	@Autowired
+	private EmployeeRepository repository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
+}
