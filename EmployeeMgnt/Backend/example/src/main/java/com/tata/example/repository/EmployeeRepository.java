42d7d48a4888836d337c00c70d826180a208d4e9
Christopher Davisson <davissonchris@gmail.com>
2022-02-01 10:30:48 -0800

    3

---
+++ EmployeeMgnt/Backend/example/src/main/java/com/tata/example/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.tata.example.repository;
+
+import com.tata.example.model.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends MongoRepository<Employee, String> {
+
+    public Employee findById(Long id);
+    public List<Employee> findAll();
+}
---
+++ EmployeeMgnt/employee-mgnt/src/app/employee/employee.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { Employee } from "./employee.model";
+import { HttpClient } from "@angular/common/http";
+
+
+@Injectable({providedIn: 'root'})
+export class EmployeeService {
+
+
+  private emps: Employee[] = [];
+
+  private employeeUpdated = new Subject<Employee[]>();
+  emp: any;
+
+  constructor(private http: HttpClient) { }
+
+  private baseURL = 'http://localhost:8080/employee/';
+
+  getEmployees() {
+    this.http.get(this.baseURL)
+    .subscribe(transEmp => {
+      let x = 0;
+      while(transEmp[x] != null) {
+        const employee: Employee = {
+          name: transEmp[x].name,
+          email: transEmp[x].email,
+          dob: transEmp[x].dob,
+          designation: transEmp[x].designation,
+          department: transEmp[x].department,
+          loginId: transEmp[x].loginId
+        };
+        this.emps.push(employee);
+        x++;
+      }
+
+      this.employeeUpdated.next([...this.emps]);
+
+    });
+  }
+  getEmployeeUpdateListener() {
+    return this.employeeUpdated.asObservable();
+  }
+
+  addEmployee(name: string, email: string, dob: Date, designation: string, department: string, loginId: string) {
+    const employee: Employee = {
+      name: name,
+      email: email,
+      dob: dob,
+      designation: designation,
+      department: department,
+      loginId: loginId
+    };
+
+    this.http.post(this.baseURL, employee).subscribe();
+    this.emps.push(employee);
+    this.employeeUpdated.next([...this.emps]);
+  }
+
+  deleteEmployee(empId: string){
+
+    this.http.delete(this.baseURL+ empId)
+    .subscribe(() => {
+      const updatedEmps = this.emps.filter(emp => emp.loginId !== empId);
+      this.emps = updatedEmps;
+      this.employeeUpdated.next([...this.emps]);
+    });
+  }
+
+  updateEmployee(employee: Employee) {
+    console.log("updateEmployee in employeeService.ts");
+    this.http.post(this.baseURL, employee).subscribe();
+  }
+
+
+}
