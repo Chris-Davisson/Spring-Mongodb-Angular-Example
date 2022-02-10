6513a92a5a98c5ce2752c3d43afd3f945a8fea74
Christopher Davisson <davissonchris@gmail.com>
2022-02-10 08:14:22 -0800

    edits to home page

--- EmployeeMgnt/employee-mgnt/src/app/app-routing.module.ts
+++ EmployeeMgnt/employee-mgnt/src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeCreateComponent } from './employee/employee-create/employee-create.component';
 import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { HomeComponent } from './home/home.component';
+
 
 const routes: Routes = [
+  { path: '' , component: HomeComponent },
   { path: 'view', component: EmployeeListComponent },
   { path: 'create', component: EmployeeCreateComponent },
 ];
